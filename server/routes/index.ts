1b1a5c7948036ce6483b90acbf6442f4c35893b4
nia evans <nia.evans@digital.justice.gov.uk>
2025-07-07 08:04:19 +0100

    Remove test page

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -3,24 +3,18 @@ import { type RequestHandler, Router } from 'express'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import type { Services } from '../services'
 import { Page } from '../services/auditService'
-import TestPageController from '../testPage/testPageController'
 import CaselistController from '../caselist/caselistController'
 
 export default function routes({ auditService, accreditedProgrammesManageAndDeliverService }: Services): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
-  const testPageController = new TestPageController(accreditedProgrammesManageAndDeliverService)
   const caselistController = new CaselistController(accreditedProgrammesManageAndDeliverService)
 
   get('/', async (req, res, next) => {
     await auditService.logPageView(Page.EXAMPLE_PAGE, { who: res.locals.user.username, correlationId: req.id })
 
     return res.render('pages/index')
-  })
-
-  get('/test', async (req, res, next) => {
-    await testPageController.showTestPage(req, res)
   })
 
   get('/pdu/open-referrals', async (req, res, next) => {
--- server/testPage/testPagePresenter.ts
+++
@@ -1,7 +0,0 @@
-import { DummyData } from '../services/accreditedProgrammesManageAndDeliverService'
-
-export default class TestPagePresenter {
-  constructor(private testData: DummyData) {
-    this.testData = testData
-  }
-}
