a6de4dbf6c1a284cfee3840d77bbd28b5cb57633
nia evans <nia.evans@digital.justice.gov.uk>
2025-07-10 13:22:54 +0100

    Set up tabs component for the personal details page

--- server/personalDetails/personalDetailsPresenter.ts
+++ server/personalDetails/personalDetailsPresenter.ts
@@ -1,7 +1,91 @@
 import PersonalDetails from '../models/PersonalDetails'
 
+export enum PersonalDetailsPageSection {
+  PersonalDetailsTab = 'personalDetails',
+  ProgrammeHistoryTab = 'programmeHistory',
+  OffenceHistoryTab = 'offenceHistory',
+  SentenceInformationTab = 'sentenceInformation',
+  AvailabilityTab = 'availability',
+  LocationTab = 'location',
+  AdditionalInformationTab = 'additionalInformation',
+}
+
 export default class PersonalDetailsPresenter {
-  constructor(private personalDetails: PersonalDetails | null) {
+  constructor(
+    private personalDetails: PersonalDetails | null,
+    readonly subNavValue: string,
+    readonly id: string,
+  ) {
     this.personalDetails = personalDetails
+  }
+
+  getHorizontalSubNavArgs(): { items: { text: string; href: string; active: boolean }[] } {
+    return {
+      items: [
+        {
+          text: 'Referral details',
+          href: `/personalDetails/${this.id}`,
+          active: true,
+        },
+        {
+          text: 'Risks and needs',
+          href: '#2',
+          active: false,
+        },
+        {
+          text: 'Programme needs identifier',
+          href: '#3',
+          active: false,
+        },
+        {
+          text: 'Status history',
+          href: '#4',
+          active: false,
+        },
+      ],
+    }
+  }
+
+  getVerticalSubNavArgs(): { items: { text: string; href: string; active: boolean }[]; classes: string } {
+    return {
+      items: [
+        {
+          text: 'Personal Details',
+          href: `/personalDetails/${this.id}?section=personalDetails`,
+          active: this.subNavValue === PersonalDetailsPageSection.PersonalDetailsTab,
+        },
+        {
+          text: 'Programme History',
+          href: `/personalDetails/${this.id}?section=programmeHistory`,
+          active: this.subNavValue === PersonalDetailsPageSection.ProgrammeHistoryTab,
+        },
+        {
+          text: 'Offence History',
+          href: `/personalDetails/${this.id}?section=offenceHistory`,
+          active: this.subNavValue === PersonalDetailsPageSection.OffenceHistoryTab,
+        },
+        {
+          text: 'Sentence Information',
+          href: `/personalDetails/${this.id}?section=sentenceInformation`,
+          active: this.subNavValue === PersonalDetailsPageSection.SentenceInformationTab,
+        },
+        {
+          text: 'Availability',
+          href: `/personalDetails/${this.id}?section=availability`,
+          active: this.subNavValue === PersonalDetailsPageSection.AvailabilityTab,
+        },
+        {
+          text: 'Location',
+          href: `/personalDetails/${this.id}?section=location`,
+          active: this.subNavValue === PersonalDetailsPageSection.LocationTab,
+        },
+        {
+          text: 'Additional Information',
+          href: `/personalDetails/${this.id}?section=additionalInformation`,
+          active: this.subNavValue === PersonalDetailsPageSection.AdditionalInformationTab,
+        },
+      ],
+      classes: 'govuk-!-padding-top-0',
+    }
   }
 }
