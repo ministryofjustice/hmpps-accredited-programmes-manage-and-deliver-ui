c289fa9652f6930ec725a3f65e912bc6f4cd1eee
Ryan Forsyth <ryan.forsyth@justice.gov.uk>
2025-07-15 15:13:00 +0100

    extract rest client creation into own method (#31)
    * extract rest client creation into own method
    
    * remove unused import
    
    ---------
    
    Co-authored-by: Ryan Forsyth <ryan.forsyth@digital.justice.gov.uk>

--- server/services/accreditedProgrammesManageAndDeliverService.ts
+++ server/services/accreditedProgrammesManageAndDeliverService.ts
@@ -1,4 +1,3 @@
-import type { SystemToken } from '@hmpps-auth'
 import type { HmppsAuthClient, RestClientBuilderWithoutToken } from '../data'
 import RestClient from '../data/restClient'
 import config, { ApiConfig } from '../config'
@@ -17,17 +16,19 @@ export interface PaginationParams {
 export default class AccreditedProgrammesManageAndDeliverService {
   constructor(private readonly hmppsAuthClientBuilder: RestClientBuilderWithoutToken<HmppsAuthClient>) {}
 
-  createRestClient = (token: Express.User['token'] | SystemToken): RestClient =>
-    new RestClient(
-      'Accredited Programmes Manage And Deliver Service API Client',
-      config.apis.accreditedProgrammesManageAndDeliverService as ApiConfig,
-      token,
-    )
-
-  async getOpenCaselist(username: Express.User['username']): Promise<Caselist> {
+  async createRestClientFromUsername(username: Express.User['username']): Promise<RestClient> {
     const hmppsAuthClient = this.hmppsAuthClientBuilder()
     const systemToken = await hmppsAuthClient.getSystemClientToken(username)
-    const restClient = this.createRestClient(systemToken)
+
+    return new RestClient(
+      'Accredited Programmes Manage And Deliver Service API Client',
+      config.apis.accreditedProgrammesManageAndDeliverService as ApiConfig,
+      systemToken,
+    )
+  }
+
+  async getOpenCaselist(username: Express.User['username']): Promise<Caselist> {
+    const restClient = await this.createRestClientFromUsername(username)
     return (await restClient.get({
       path: `/pages/caselist/open`,
       headers: { Accept: 'application/json' },
@@ -35,9 +36,7 @@ export default class AccreditedProgrammesManageAndDeliverService {
   }
 
   async getClosedCaselist(username: Express.User['username']): Promise<Caselist> {
-    const hmppsAuthClient = this.hmppsAuthClientBuilder()
-    const systemToken = await hmppsAuthClient.getSystemClientToken(username)
-    const restClient = this.createRestClient(systemToken)
+    const restClient = await this.createRestClientFromUsername(username)
     return (await restClient.get({
       path: `/pages/caselist/closed`,
       headers: { Accept: 'application/json' },
@@ -45,9 +44,7 @@ export default class AccreditedProgrammesManageAndDeliverService {
   }
 
   async getPersonalDetails(username: Express.User['username'], id: string): Promise<PersonalDetails> {
-    const hmppsAuthClient = this.hmppsAuthClientBuilder()
-    const systemToken = await hmppsAuthClient.getSystemClientToken(username)
-    const restClient = this.createRestClient(systemToken)
+    const restClient = await this.createRestClientFromUsername(username)
     return (await restClient.get({
       path: `/referral/${id}`,
       headers: { Accept: 'application/json' },
