b9e7471eb54c123203d28fb40ed2368258f211da
niaevans9 <nia.evans@digital.justice.gov.uk>
2025-07-16 15:07:36 +0100

    WIP offence history screen (#36)

--- server/referralDetails/referralDetailsPresenter.ts
+++ server/referralDetails/referralDetailsPresenter.ts
@@ -175,4 +175,60 @@ export default class ReferralDetailsPresenter {
       },
     ]
   }
+
+  offenceHistorySummaryLists(): { title: string; summary: SummaryListItem[] }[] {
+    const summaries = []
+    summaries.push({
+      title: 'Index Offence',
+      summary: [
+        {
+          key: 'Offence',
+          lines: ['Publishing or causing to be published a tobacco advertisement - 09144'],
+        },
+        {
+          key: 'Category',
+          lines: ['Public Health Offences'],
+        },
+        {
+          key: 'Offence date',
+          lines: ['11 June 2020'],
+        },
+      ],
+    })
+    summaries.push({
+      title: 'Additional offence (08000)',
+      summary: [
+        {
+          key: 'Offence',
+          lines: ['Absconding from lawful custody - 08000'],
+        },
+        {
+          key: 'Category',
+          lines: ['Absconding from lawful custody'],
+        },
+        {
+          key: 'Offence date',
+          lines: ['18 January 2013'],
+        },
+      ],
+    })
+    summaries.push({
+      title: 'Additional offence (08000)',
+      summary: [
+        {
+          key: 'Offence',
+          lines: ['\tClass unspecified - permitting premises to be used - 09329'],
+        },
+        {
+          key: 'Category',
+          lines: ['Permitting premises to be used for unlawful (drug-related) purposes'],
+        },
+        {
+          key: 'Offence date',
+          lines: ['23 September 2000'],
+        },
+      ],
+    })
+    return summaries
+  }
 }
