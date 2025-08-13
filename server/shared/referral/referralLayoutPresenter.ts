a0f9581e1c65aeefbfd4de0c51177789079206f7
Ryan Forsyth <ryan.forsyth@digital.justice.gov.uk>
2025-08-13 11:36:21 +0100

    navigation for referral pages

---
+++ server/shared/referral/referralLayoutPresenter.ts
@@ -0,0 +1,59 @@
+export enum HorizontalNavValues {
+  referralDetailsTab = 'referralDetails',
+  risksAndNeedsTab = 'risksAndNeeds',
+  programmeNeedsIdentifierTab = 'programmeNeedsIdentifier',
+  statusHistoryTab = 'statusHistory',
+}
+
+export default class ReferralLayoutPresenter {
+  protected constructor(
+    readonly horizontalNavValue: HorizontalNavValues,
+    readonly referralId: string,
+  ) {}
+
+  getSubHeaderArgs(): {
+    heading: { text: string; classes: string }
+    items: { text: string; classes: string; href?: string }[]
+  } {
+    return {
+      heading: {
+        text: 'Referral to Building Choices: moderate intensity',
+        classes: 'govuk-heading-l',
+      },
+      items: [
+        {
+          text: 'Back to referrals',
+          classes: 'govuk-button--secondary',
+          href: '/pdu/open-referrals',
+        },
+      ],
+    }
+  }
+
+  getHorizontalSubNavArgs(): { items: { text: string; href: string; active: boolean }[] } {
+    return {
+      items: [
+        {
+          text: 'Referral details',
+          href: `/referral-details/${this.referralId}/personal-details`,
+          active: this.horizontalNavValue === HorizontalNavValues.referralDetailsTab,
+        },
+        {
+          text: 'Risks and needs',
+          href: `/referral/${this.referralId}/risks-and-needs`,
+          active: this.horizontalNavValue === HorizontalNavValues.risksAndNeedsTab,
+        },
+        {
+          text: 'Programme needs identifier',
+          href: '#3',
+          active: this.horizontalNavValue === HorizontalNavValues.programmeNeedsIdentifierTab,
+        },
+        {
+          text: 'Status history',
+          href: '#4',
+          active: this.horizontalNavValue === HorizontalNavValues.statusHistoryTab,
+        },
+      ],
+    }
+  }
+}
