47b05f8e461ce7d804709745433f3320498a8029
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2025-08-13 17:03:49 +0100

    [APG-1123] Fix filtering in caselist view

--- server/caselist/caselistFilter.ts
+++ server/caselist/caselistFilter.ts
@@ -19,13 +19,13 @@ export default class CaselistFilter {
   get params(): CaselistFilterParams {
     const params: CaselistFilterParams = {}
 
-    if (this.status !== undefined && this.status !== '') {
+    if (this.status) {
       params.status = this.status
     }
-    if (this.cohort !== undefined && this.cohort !== '') {
+    if (this.cohort) {
       params.cohort = this.cohort
     }
-    if (this.crnOrPersonName !== undefined && this.crnOrPersonName.trim() !== '') {
+    if (this.crnOrPersonName?.trim()) {
       params.crnOrPersonName = this.crnOrPersonName.trim()
     }
 
