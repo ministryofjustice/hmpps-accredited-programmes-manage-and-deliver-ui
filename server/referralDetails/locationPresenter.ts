35f188eb7078770278ac3b0f60f0925e0d855fca
nia evans <nia.evans@digital.justice.gov.uk>
2025-09-09 10:34:08 +0100

    Adding in location preferences UI

--- server/referralDetails/locationPresenter.ts
+++ server/referralDetails/locationPresenter.ts
@@ -1,5 +1,6 @@
 import { ReferralDetails } from '@manage-and-deliver-api'
 import ReferralDetailsPresenter from './referralDetailsPresenter'
+import { SummaryListItem } from '../utils/summaryList'
 
 export default class LocationPresenter extends ReferralDetailsPresenter {
   constructor(
@@ -8,5 +9,41 @@ export default class LocationPresenter extends ReferralDetailsPresenter {
     readonly id: string,
   ) {
     super(details, subNavValue, id)
+  }
+
+  ReportingLocationsSummary(): { title: string; summary: SummaryListItem[] } {
+    return {
+      title: 'Reporting locations',
+      summary: [
+        {
+          key: 'Probation delivery unit',
+          lines: [`East Sussex`],
+        },
+        {
+          key: 'Probation office',
+          lines: ['Brighton and Hove: Brighton Probation Office'],
+        },
+      ],
+    }
+  }
+
+  PreferredLocationsSummary(): { title: string; classes: string; summary: SummaryListItem[] } {
+    return {
+      title: 'Reporting locations',
+      classes: 'no-first-key',
+      summary: [
+        {
+          lines: [`Last updated 3 April 2025 by Tom Saunders`],
+        },
+        {
+          key: 'Preferred programme delivery locations',
+          lines: [`East Sussex`],
+        },
+        {
+          key: 'Locations the person cannot attend',
+          lines: ['Brighton and Hove: Brighton Probation Office'],
+        },
+      ],
+    }
   }
 }
--- server/referralDetails/locationView.ts
+++ server/referralDetails/locationView.ts
@@ -1,13 +1,33 @@
 import LocationPresenter from './locationPresenter'
+import { SummaryListArgs } from '../utils/govukFrontendTypes'
+import ViewUtils from '../utils/viewUtils'
 
 export default class LocationView {
   constructor(private readonly presenter: LocationPresenter) {}
+
+  get reportingLocationsSummary(): SummaryListArgs {
+    const summary = this.presenter.ReportingLocationsSummary()
+    return ViewUtils.summaryListArgsWithSummaryCard(summary.summary, summary.title)
+  }
+
+  get preferredLocationsSummary(): SummaryListArgs {
+    const summary = this.presenter.PreferredLocationsSummary()
+    return ViewUtils.summaryListArgsWithSummaryCard(
+      summary.summary,
+      summary.title,
+      { showBorders: true, showTitle: true, hideKey: false },
+      { href: '', text: 'Add location preferences', visuallyHiddenText: '' },
+      summary.classes,
+    )
+  }
 
   get renderArgs(): [string, Record<string, unknown>] {
     return [
       'referralDetails/referralDetails',
       {
         presenter: this.presenter,
+        reportingLocationsSummary: this.reportingLocationsSummary,
+        preferredLocationsSummary: this.preferredLocationsSummary,
       },
     ]
   }
