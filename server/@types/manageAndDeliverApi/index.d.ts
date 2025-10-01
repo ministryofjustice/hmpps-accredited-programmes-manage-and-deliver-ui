edc8c089aa0ef189709bbc0b87e5ec95218aebb0
Thomas Wilson-Cook <thomas.wilson-cook@justice.gov.uk>
2025-10-01 15:48:41 +0100

    chore: add ReferralStatusHistory type

--- server/@types/manageAndDeliverApi/index.d.ts
+++ server/@types/manageAndDeliverApi/index.d.ts
@@ -34,6 +34,7 @@ type ReferralStatusFormData = components['schemas']['ReferralStatusFormData']
 type CreateReferralStatusHistory = components['schemas']['CreateReferralStatusHistory']
 type ReferralStatus = components['schemas']['ReferralStatus']
 type CaseListFilterValues = components['schemas']['CaseListFilterValues']
+type ReferralStatusHistory = components['schemas']['ReferralStatusHistory']
 
 export type {
   AlcoholMisuseDetails,
@@ -62,6 +63,7 @@ export type {
   ReferralDetails,
   ReferralStatus,
   ReferralStatusFormData,
+  ReferralStatusHistory,
   Relationships,
   Risks,
   RoshAnalysis,
