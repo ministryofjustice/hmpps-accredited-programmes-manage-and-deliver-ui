f16abc8185add9cb8292a8b7960c81af0084440a
Alison Weatherston <alison.weatherston@justice.gov.uk>
2025-10-16 14:52:28 +0100

    eslint config revert

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
-const base = Array.isArray(hmppsConfig) ? hmppsConfig : [hmppsConfig]
-
 export default [
-  ...base,
-  { ignores: ['node_modules', 'public', 'assets', 'reporter-config.json', 'dist', 'test_results'] },
+  ...hmppsConfig(),
+
+  {
+    ignores: ['node_modules', 'public', 'assets', 'reporter-config.json', 'dist', 'test_results'],
+  },
 ]
