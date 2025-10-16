2983a3f25b554811cc96ae8a06cb3d8d3c381c6e
Alison Weatherston <alison.weatherston@justice.gov.uk>
2025-10-16 14:35:10 +0100

    eslint changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
 export default [
-  hmppsConfig(),
+  ...hmppsConfig(),
 
+  // then your local tweaks
   {
     ignores: ['node_modules', 'public', 'assets', 'reporter-config.json', 'dist', 'test_results'],
   },
