cd06de3038d858285047b6398bc51cb03b942126
Alison Weatherston <alison.weatherston@justice.gov.uk>
2025-10-21 11:09:40 +0100

    eslintignore removed and code moved to eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
 import hmppsConfig from '@ministryofjustice/eslint-config-hmpps'
 
-export default hmppsConfig()
+export default [
+  ...hmppsConfig(),
+
+  {
+    ignores: ['node_modules', 'public', 'assets', 'reporter-config.json', 'dist', 'test_results'],
+  },
+]
