4140c4078578e31b06ffdb2a1246fc4a20a826e5
Alison Weatherston <alison.weatherston@justice.gov.uk>
2025-10-21 10:22:11 +0100

    delete

--- jest.setup.ts
+++
@@ -1,5 +0,0 @@
-import { faker } from '@faker-js/faker'
-
-// Make faker deterministic (stops snapshot/brittle failures)
-faker.seed(12345)
-faker.setDefaultRefDate(new Date('2020-01-01T00:00:00Z'))
