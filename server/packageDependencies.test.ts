056682202fbf368930cd1a4c4a66afaad291579c
Alison Weatherston <alison.weatherston@justice.gov.uk>
2025-10-21 14:03:30 +0100

    bunyan v2

--- server/packageDependencies.test.ts
+++ server/packageDependencies.test.ts
@@ -1,8 +1,8 @@
 import fs from 'fs'
 
 describe('app insights compatibility', () => {
-  it('uses bunyan v1', () => {
-    // See https://github.com/Microsoft/node-diagnostic-channel/blob/master/src/diagnostic-channel-publishers/README.md
+  it('uses bunyan v2', () => {
+    // See https://github.com/microsoft/node-diagnostic-channel/tree/main/src/diagnostic-channel-publishers
     const packageData = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
 
     expect(packageData.dependencies.bunyan).toMatch(/^[~^]?2\./)
