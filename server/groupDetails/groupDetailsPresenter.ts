088b977bbaa7f24d1f582d278ff64b9531709823
Alison Weatherston <alison.weatherston@justice.gov.uk>
2025-10-31 11:36:34 +0000

    initial commit

--- server/groupDetails/groupDetailsPresenter.ts
+++ server/groupDetails/groupDetailsPresenter.ts
@@ -38,6 +38,7 @@ export default class GroupDetailsPresenter {
     readonly group: ProgrammeGroupDetails,
     readonly groupId: string,
     readonly isPersonAdded: boolean | null = null,
+    public readonly addedPersonName?: string,
   ) {}
 
   private groupMemberList: (AllocatedRow | WaitlistRow)[] = []
@@ -46,10 +47,15 @@ export default class GroupDetailsPresenter {
     this.groupMemberList = rows ?? []
   }
 
-  readonly text = {
-    pageHeading: `North East`,
-    pageSubHeading: `BCCDD1`,
-    pageTableHeading: `Allocations and waitlist`,
+  get text() {
+    console.log('XXXXXXXX', {
+      group: this.group?.group,
+    })
+    return {
+      pageHeading: this.group.group.regionName,
+      pageSubHeading: this.group.group.code,
+      pageTableHeading: 'Allocations and waitlist',
+    }
   }
 
   getSubNavArgs(): { items: { text: string; href: string; active: boolean }[] } {
