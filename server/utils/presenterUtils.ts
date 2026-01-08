968dfe6ed6c025530e9218461b15b433bda05c2b
niaevans9 <nia.evans@digital.justice.gov.uk>
2026-01-08 14:01:50 +0000

    New component to capture time values (#294)
    * New component to capture time values
    
    * Removing unused import
    
    * Support multiple errors

--- server/utils/presenterUtils.ts
+++ server/utils/presenterUtils.ts
@@ -1,3 +1,22 @@
+import { FormValidationError } from './formValidationError'
+
+interface DateTimeComponentInputPresenter {
+  value: string
+  hasError: boolean
+}
+
+interface PartOfDayInputPresenter {
+  value: 'AM' | 'PM' | null
+  hasError: boolean
+}
+
+interface TwelveHourTimeInputPresenter {
+  errorMessages: string[] | null
+  hour: DateTimeComponentInputPresenter
+  minute: DateTimeComponentInputPresenter
+  partOfDay: PartOfDayInputPresenter
+}
+
 export default class PresenterUtils {
   constructor(private readonly userInputData: Record<string, unknown> | null = null) {}
 
@@ -80,5 +99,61 @@ export default class PresenterUtils {
       return 'Unknown'
     }
     return value ? 'Yes' : 'No'
+  }
+
+  twelveHourTimeValue(
+    modelValue: {
+      hour?: number
+      minutes?: number
+      amOrPm?: 'AM' | 'PM'
+    },
+    userInputKey: string,
+    error: FormValidationError | null,
+  ): TwelveHourTimeInputPresenter {
+    const [hourKey, minuteKey, partOfDayKey] = ['hour', 'minute', 'part-of-day'].map(
+      suffix => `${userInputKey}-${suffix}`,
+    )
+
+    const errorMessages = [
+      PresenterUtils.errorMessage(error, hourKey),
+      PresenterUtils.errorMessage(error, minuteKey),
+      PresenterUtils.errorMessage(error, partOfDayKey),
+    ].filter((msg): msg is string => msg !== null)
+
+    let hourValue = ''
+    let minuteValue = ''
+    let partOfDayValue: 'AM' | 'PM' | null = null
+
+    if (this.userInputData !== null) {
+      hourValue = String(this.userInputData[hourKey] || '')
+      minuteValue = String(this.userInputData[minuteKey] || '')
+
+      if (this.userInputData[partOfDayKey] === 'AM' || this.userInputData[partOfDayKey] === 'PM') {
+        partOfDayValue = this.userInputData[partOfDayKey] as 'AM' | 'PM'
+      }
+    } else if (
+      modelValue !== null &&
+      (modelValue.hour !== undefined || modelValue.minutes !== undefined || modelValue.amOrPm !== undefined)
+    ) {
+      hourValue = modelValue.hour !== undefined ? modelValue.hour.toString().padStart(2, '0') : ''
+      minuteValue = modelValue.minutes !== undefined ? modelValue.minutes.toString().padStart(2, '0') : ''
+      partOfDayValue = modelValue.amOrPm ?? null
+    }
+
+    return {
+      errorMessages,
+      partOfDay: {
+        value: partOfDayValue,
+        hasError: PresenterUtils.hasError(error, partOfDayKey),
+      },
+      hour: {
+        value: hourValue,
+        hasError: PresenterUtils.hasError(error, hourKey),
+      },
+      minute: {
+        value: minuteValue,
+        hasError: PresenterUtils.hasError(error, minuteKey),
+      },
+    }
   }
 }
