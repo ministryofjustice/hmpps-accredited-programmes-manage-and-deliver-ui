32b745a7cb2b3f21518a30e2432ae71fbc97bf8b
Alison Weatherston <alison.weatherston@justice.gov.uk>
2026-02-10 08:31:10 +0000

    EditSessionAttendee unused

--- server/services/accreditedProgrammesManageAndDeliverService.ts
+++ server/services/accreditedProgrammesManageAndDeliverService.ts
@@ -48,7 +48,6 @@ import {
   ThinkingAndBehaviour,
   UpdateAvailability,
   UserTeamMember,
-  EditSessionAttendee,
   EditSessionAttendeesResponse,
   EditSessionDetails,
   RescheduleSessionDetails,
