0b207f43a6d25b46438cfa6d6577c5066251daff
Alison Weatherston <alison.weatherston@justice.gov.uk>
2026-02-12 15:48:13 +0000

    comment removed

--- server/editSession/dateAndTime/editSessionDateAndTimeForm.ts
+++ server/editSession/dateAndTime/editSessionDateAndTimeForm.ts
@@ -16,9 +16,6 @@ const fieldOrder = [
   'session-details-who',
   'session-details-facilitator',
 ]
-
-// TEST
-
 export default class EditSessionDateAndTimeFormForm {
   constructor(private readonly request: Request) {}
 
