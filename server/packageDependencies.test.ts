841f2d65e48db06ec92a4720863c3ed648fa1aa2
Alison-Weatherston <alison.weatherston@justice.gov.uk>
2026-02-23 14:29:04 +0000

    package dependencies revert

--- server/packageDependencies.test.ts
+++ server/packageDependencies.test.ts
@@ -4,6 +4,7 @@ describe('app insights compatibility', () => {
   it('uses bunyan v1', () => {
     // See https://github.com/Microsoft/node-diagnostic-channel/blob/master/src/diagnostic-channel-publishers/README.md
     const packageData = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
-    expect(packageData.dependencies.bunyan).toMatch(/^\^?1\..*/)
+    // eslint-disable-next-line no-useless-escape
+    expect(packageData.dependencies.bunyan).toMatch(/[^\.]1\..*/)
   })
 })
