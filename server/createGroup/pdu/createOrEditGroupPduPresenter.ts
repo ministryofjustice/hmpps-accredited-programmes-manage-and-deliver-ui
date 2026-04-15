beae29493a65ca83215cf1769fb3fef571921880
niaevans9 <nia.evans@digital.justice.gov.uk>
2026-04-15 15:49:54 +0100

    Edit a groups PDU and location (#453)
    * Edit a groups PDU and location
    
    * Update button text

--- server/createGroup/pdu/createOrEditGroupPduPresenter.ts
+++ server/createGroup/pdu/createOrEditGroupPduPresenter.ts
@@ -3,20 +3,29 @@ import { FormValidationError } from '../../utils/formValidationError'
 import PresenterUtils from '../../utils/presenterUtils'
 import { SelectArgsItem } from '../../utils/govukFrontendTypes'
 
-export default class CreateGroupPduPresenter {
+export default class CreateOrEditGroupPduPresenter {
   constructor(
     readonly locations: CodeDescription[],
     private readonly validationError: FormValidationError | null = null,
     private readonly createGroupFormData: Partial<CreateGroupRequest> | null = null,
     private readonly userInputData: Record<string, unknown> | null = null,
+    private readonly groupId: string | null = null,
+    readonly isEdit = false,
   ) {}
 
   get backLinkUri() {
-    return `/group/create-a-group/group-sex`
+    return this.isEdit ? `/group/${this.groupId}/group-details` : `/group/create-a-group/group-sex`
   }
 
   get text() {
-    return { headingHintText: `Create group ${this.createGroupFormData?.groupCode}` }
+    return {
+      headingHintText: this.isEdit
+        ? `Edit group ${this.createGroupFormData?.groupCode}`
+        : `Create group ${this.createGroupFormData?.groupCode}`,
+      headingText: this.isEdit
+        ? `Edit the probation delivery unit (PDU) where the group will take place`
+        : `In which probation delivery unit (PDU) will the group take place?`,
+    }
   }
 
   get errorSummary() {
