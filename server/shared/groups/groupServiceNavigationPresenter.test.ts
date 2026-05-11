15f6f1cb2a7ee9bdfe850961baa51412290b57e2
Alison-Weatherston <alison.weatherston@justice.gov.uk>
2026-05-11 14:23:13 +0100

    test added

--- server/shared/groups/groupServiceNavigationPresenter.test.ts
+++ server/shared/groups/groupServiceNavigationPresenter.test.ts
@@ -1,22 +1,93 @@
 import { randomUUID } from 'crypto'
 import SchedulePresenter from '../../groupOverview/schedule/schedulePresenter'
+import GroupDetailsPresenter from '../../groupDetails/groupDetailsPresenter'
+import GroupAllocationsPresenter, {
+  GroupAllocationsPageSection,
+} from '../../groupOverview/allocations/groupAllocationsPresenter'
+import SessionScheduleAttendancePresenter from '../../sessionSchedule/sessionAttendance/sessionScheduleAttendancePresenter'
+import GroupDetailsFactory from '../../testutils/factories/groupDetailsFactory'
+import ProgrammeGroupOverviewFactory from '../../testutils/factories/programmeGroupAllocationsFactory'
+import GroupAllocationsFilter from '../../groupOverview/allocations/groupAllocationsFilter'
 
 describe('GroupServiceNavigationPresenter', () => {
-  const groupId = randomUUID()
-
   afterEach(() => {
     jest.restoreAllMocks()
   })
 
   describe('getMojSubNavigationArgs', () => {
+    it('should return navigation args with group details active', () => {
+      const groupDetails = GroupDetailsFactory.build()
+      const presenter = new GroupDetailsPresenter(groupDetails)
+
+      expect(presenter.getMojSubNavigationArgs()).toEqual({
+        items: [
+          {
+            href: `/group/${groupDetails.id}/group-details`,
+            text: 'Group details',
+            active: true,
+          },
+          {
+            href: `/group/${groupDetails.id}/allocations`,
+            text: 'Allocations and waitlist',
+            active: false,
+          },
+          {
+            href: `/group/${groupDetails.id}/schedule-overview`,
+            text: 'Schedule overview',
+            active: false,
+          },
+          {
+            href: `/group/${groupDetails.id}/sessions-and-attendance`,
+            text: 'Sessions and attendance',
+            active: false,
+          },
+        ],
+      })
+    })
+
     it('should return navigation args with allocations active', () => {
-      // Used as an example to test the schedule tab, as service navigation presenter is protected.
+      const groupAllocations = ProgrammeGroupOverviewFactory.build()
+      const presenter = new GroupAllocationsPresenter(
+        GroupAllocationsPageSection.Allocated,
+        groupAllocations,
+        groupAllocations.group.id,
+        GroupAllocationsFilter.empty(),
+      )
+
+      expect(presenter.getMojSubNavigationArgs()).toEqual({
+        items: [
+          {
+            href: `/group/${groupAllocations.group.id}/group-details`,
+            text: 'Group details',
+            active: false,
+          },
+          {
+            href: `/group/${groupAllocations.group.id}/allocations`,
+            text: 'Allocations and waitlist',
+            active: true,
+          },
+          {
+            href: `/group/${groupAllocations.group.id}/schedule-overview`,
+            text: 'Schedule overview',
+            active: false,
+          },
+          {
+            href: `/group/${groupAllocations.group.id}/sessions-and-attendance`,
+            text: 'Sessions and attendance',
+            active: false,
+          },
+        ],
+      })
+    })
+
+    it('should return navigation args with schedule overview active', () => {
+      const groupId = randomUUID()
       const presenter = new SchedulePresenter(groupId, {
         preGroupOneToOneStartDate: '',
         gettingStartedModuleStartDate: '',
         endDate: '',
         sessions: [],
-        code: '',
+        code: 'TEST-001',
       })
 
       expect(presenter.getMojSubNavigationArgs()).toEqual({
@@ -40,6 +111,36 @@ describe('GroupServiceNavigationPresenter', () => {
             href: `/group/${groupId}/sessions-and-attendance`,
             text: 'Sessions and attendance',
             active: false,
+          },
+        ],
+      })
+    })
+
+    it('should return navigation args with sessions and attendance active', () => {
+      const testGroupId = randomUUID()
+      const presenter = new SessionScheduleAttendancePresenter(testGroupId)
+
+      expect(presenter.getMojSubNavigationArgs()).toEqual({
+        items: [
+          {
+            href: `/group/${testGroupId}/group-details`,
+            text: 'Group details',
+            active: false,
+          },
+          {
+            href: `/group/${testGroupId}/allocations`,
+            text: 'Allocations and waitlist',
+            active: false,
+          },
+          {
+            href: `/group/${testGroupId}/schedule-overview`,
+            text: 'Schedule overview',
+            active: false,
+          },
+          {
+            href: `/group/${testGroupId}/sessions-and-attendance`,
+            text: 'Sessions and attendance',
+            active: true,
           },
         ],
       })
