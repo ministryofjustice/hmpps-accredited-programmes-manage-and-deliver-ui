2a5cb540a50495ffe68d8f2d7230d704abf63c97
Alison-Weatherston <alison.weatherston@justice.gov.uk>
2026-05-22 11:21:50 +0100

    Apg 2258/update page titles and check urls add attendance and session notes (#543)
    * Updated URLs and page titles for Group details
    
    * Update page titles and URl for attendace and session notes
    
    * updates
    
    * remove script
    
    * updated name
    
    * updates
    
    * some minor adjustments
    
    * updates to backlink

--- server/sessionNotes/sessionNotesPresenter.ts
+++ server/sessionNotes/sessionNotesPresenter.ts
@@ -20,7 +20,7 @@ export default class SessionNotesPresenter {
   ) {}
 
   get pageTitle(): string {
-    return `${this.data.moduleName} ${this.data.sessionNumber} attendance and session notes`
+    return `${this.data.personOnProbationName} record attendance and progress`
   }
 
   get text() {
