863925c0ea784aba43ff585f90f46d37da5e09c2
Alison-Weatherston <alison.weatherston@justice.gov.uk>
2026-05-26 16:40:38 +0100

    utils

--- server/editSession/editSessionController.test.ts
+++ server/editSession/editSessionController.test.ts
@@ -81,7 +81,6 @@ describe('editSession', () => {
         expect(res.text).toContain('<title>Getting started one-to-one - Accredited Programmes</title>')
         expect(res.text).toContain('Getting started one-to-one')
         expect(res.text).toContain(`/${groupId}/${sessionId}/getting-started-one-to-one-attendance-and-session-notes`)
- 
       })
   })
 
