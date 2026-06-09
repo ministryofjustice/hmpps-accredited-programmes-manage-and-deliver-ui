0998e24b7c2db665c1af260ead88d46fd34fa58e
nia evans <nia.evans@digital.justice.gov.uk>
2026-06-09 13:22:45 +0100

    Re-order headings for accessibility

--- server/createGroup/region/createOrEditGroupPduView.ts
+++ server/createGroup/region/createOrEditGroupPduView.ts
@@ -17,13 +17,9 @@ export default class CreateOrEditGroupPduView {
       id: 'create-group-pdu',
       name: 'create-group-pdu',
       label: {
-        text: this.presenter.text.headingText,
-        classes: 'govuk-label--l',
-        isPageHeading: true,
-      },
-      hint: {
         text: 'Search for PDU',
-        classes: 'govuk-label--m govuk-label govuk-!-margin-top-6',
+        classes: 'govuk-label--m',
+        isPageHeading: false,
       },
       errorMessage: ViewUtils.govukErrorMessage(this.presenter.fields.createGroupPdu.errorMessage),
       items: this.presenter.generateSelectOptions(),
