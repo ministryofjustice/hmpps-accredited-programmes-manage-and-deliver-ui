004bfe307bdc69c875c65d0cd309ea9f74b79bd6
Alison-Weatherston <alison.weatherston@justice.gov.uk>
2026-06-22 15:15:18 +0100

    Add test coverage for 500 and 503 error pages and test routes

--- server/errorHandler.test.ts
+++ server/errorHandler.test.ts
@@ -13,22 +13,8 @@ afterEach(() => {
 })
 
 describe('GET 404', () => {
-  it('should render content without stack in dev mode', () => {
+  it('should render 404 content without debug details', () => {
     return request(app)
-      .get('/unknown')
-      .expect(404)
-      .expect('Content-Type', /html/)
-      .expect(res => {
-        expect(res.text).toContain('Page not found')
-        expect(res.text).toContain('If you typed the web address, check it is correct.')
-        expect(res.text).toContain('Case list')
-        expect(res.text).not.toContain('NotFoundError: Not Found')
-        expect(res.text).not.toContain('Not found')
-      })
-  })
-
-  it('should render content without stack in production mode', () => {
-    return request(appWithAllRoutes({ production: true }))
       .get('/unknown')
       .expect(404)
       .expect('Content-Type', /html/)
@@ -43,7 +29,7 @@ describe('GET 404', () => {
 })
 
 describe('GET 500', () => {
-  it('should render 500 heading and body without debug details', () => {
+  it('should render 500 heading and body without debug details but with navigation', () => {
     return request(app)
       .get('/route-that-throws-500')
       .expect(500)
@@ -53,28 +39,31 @@ describe('GET 500', () => {
         expect(res.text).toContain('Try reloading the page')
         expect(res.text).toContain('Tech Portal')
         expect(res.text).not.toContain('Test 500 error') // error message hidden
+        expect(res.text).toContain('Case list') // navigation visible
       })
   })
 })
 
 describe('GET 503', () => {
-  it('should render 503 heading in dev mode', () => {
+  it('should render 503 heading without navigation', () => {
     return request(app)
       .get('/route-that-returns-503')
       .expect(503)
       .expect('Content-Type', /html/)
       .expect(res => {
         expect(res.text).toContain('Sorry, the service is unavailable')
+        expect(res.text).not.toContain('Case list') // navigation hidden
       })
   })
 
-  it('should render 503 heading in production mode', () => {
+  it('should render 503 heading in production mode without navigation', () => {
     return request(appWithAllRoutes({ production: true }))
       .get('/route-that-returns-503')
       .expect(503)
       .expect('Content-Type', /html/)
       .expect(res => {
         expect(res.text).toContain('Sorry, the service is unavailable')
+        expect(res.text).not.toContain('Case list') // navigation hidden
       })
   })
 })
