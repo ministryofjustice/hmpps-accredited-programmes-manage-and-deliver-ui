0aaccdd36dcc6ed95e7187e04e2a9148969c1796
DanEgan1 <55484338+DanEgan1@users.noreply.github.com>
2026-06-25 11:24:02 +0100

    Apg 2350/block unapproved regions from homepage (#612)
    * Added block for none approved regions
    
    * added tests
    
    * added test
    
    * lint fix
    
    * review chagnes
    
    * review chagnes
    
    * change flags
    
    * change flags

---
+++ server/home/homeController.test.ts
@@ -0,0 +1,111 @@
+import request from 'supertest'
+import { Express } from 'express'
+import { SessionData } from 'express-session'
+import TestUtils from '../testutils/testUtils'
+
+let app: Express
+
+describe('HomeController', () => {
+  describe('When region restriction is enabled', () => {
+    beforeAll(() => {
+      process.env.ENABLE_REGION_RESTRICTION = 'true'
+    })
+    afterAll(() => {
+      delete process.env.ENABLE_REGION_RESTRICTION
+    })
+
+    it('renders the home page when region is allowed', async () => {
+      const sessionData: Partial<SessionData> = {
+        userRegion: { regionCode: 'N53', regionDescription: 'Allowed region' },
+      }
+      app = TestUtils.createTestAppWithSession(sessionData, {})
+
+      return request(app)
+        .get('/')
+        .expect(200)
+        .expect(res => {
+          expect(res.text).toContain('Accredited Programmes')
+          expect(res.text).not.toContain('You cannot access Accredited Programmes services yet')
+        })
+    })
+
+    it('renders the invalid region page when region is not allowed', async () => {
+      const sessionData: Partial<SessionData> = {
+        userRegion: { regionCode: 'UNKNOWN', regionDescription: 'Not allowed' },
+      }
+      app = TestUtils.createTestAppWithSession(sessionData, {})
+
+      return request(app)
+        .get('/')
+        .expect(403)
+        .expect(res => {
+          expect(res.text).toContain('You cannot access Accredited Programmes services yet')
+        })
+    })
+
+    it('renders the invalid region page when region is not in session', async () => {
+      const sessionData: Partial<SessionData> = {
+        userRegion: undefined,
+      }
+      app = TestUtils.createTestAppWithSession(sessionData, {})
+
+      return request(app)
+        .get('/')
+        .expect(403)
+        .expect(res => {
+          expect(res.text).toContain('You cannot access Accredited Programmes services yet')
+        })
+    })
+  })
+
+  describe('When region restriction is disabled', () => {
+    beforeAll(() => {
+      delete process.env.ENABLE_REGION_RESTRICTION
+    })
+
+    it('renders the home page when region is allowed', async () => {
+      const sessionData: Partial<SessionData> = {
+        userRegion: { regionCode: 'N53', regionDescription: 'Allowed region' },
+      }
+      app = TestUtils.createTestAppWithSession(sessionData, {})
+
+      return request(app)
+        .get('/')
+        .expect(200)
+        .expect(res => {
+          expect(res.text).toContain('Accredited Programmes')
+          expect(res.text).not.toContain('You cannot access Accredited Programmes services yet')
+        })
+    })
+
+    it('renders the home page for any region when restriction disabled', async () => {
+      const sessionData: Partial<SessionData> = {
+        userRegion: { regionCode: 'UNKNOWN', regionDescription: 'Not allowed' },
+      }
+      app = TestUtils.createTestAppWithSession(sessionData, {})
+
+      return request(app)
+        .get('/')
+        .expect(200)
+        .expect(res => {
+          expect(res.text).toContain('Accredited Programmes')
+          expect(res.text).not.toContain('You cannot access Accredited Programmes services yet')
+        })
+    })
+
+    it('renders the home page if region is not in session', async () => {
+      const sessionData: Partial<SessionData> = {
+        userRegion: undefined,
+      }
+      app = TestUtils.createTestAppWithSession(sessionData, {})
+
+      return request(app)
+        .get('/')
+        .expect(200)
+        .expect(res => {
+          expect(res.text).toContain('Accredited Programmes')
+          expect(res.text).not.toContain('You cannot access Accredited Programmes services yet')
+        })
+    })
+  })
+})
