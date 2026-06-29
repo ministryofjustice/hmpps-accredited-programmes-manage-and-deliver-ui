4c8a21891174fc2666fbd3e40d9c79e00fb808ca
DanEgan1 <daniel.egan1@justice.gov.uk>
2026-06-29 13:56:07 +0100

    review comments

--- server/home/allowedRegions.ts
+++ server/home/allowedRegions.ts
@@ -1,11 +1,20 @@
 import config from "../config"
 
 /**
- * For info: N53 is the east midlands, N54 is the North East.
+ * For info:
+ * N53 Is the East Midlands
+ * N54 Is the North East
  * Full region code mapping can be found at:
  * https://dsdmoj.atlassian.net/wiki/spaces/IC/pages/6027477185/Interventions+Manager+Data+Migration+Strategy#2.-Phased-out-Data-Migration
  */
 export const ALLOWED_REGIONS: string[] = ['N53', 'N54']
+
+/**
+ * For info:
+ * N02 Is an alternate region code for the north east used in test Delius accounts, but not used in production.
+ * N07 Is the region code for London. Currently used for test accounts but not yet allowed in production.
+ * Other are descibed in ALLOWED_REGIONS above.
+ */
 export const DEV_ALLOWED_REGIONS: string[] = ['N01', 'N07', 'N53', 'N54'] // Dev users do not have access to the same regions in delius so extra ones allowed.
 
 /**
