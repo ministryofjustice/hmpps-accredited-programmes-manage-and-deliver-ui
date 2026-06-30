a23c5dd87b02d86875828556d8a93b7efec71a62
DanEgan1 <55484338+DanEgan1@users.noreply.github.com>
2026-06-30 13:28:23 +0100

    handle dev regions (#628)
    * handle dev regions
    
    * handle dev regions
    
    * review comments
    
    * review comments
    
    * review comments
    
    * review comments
    
    * review comments
    
    * review comments

---
+++ server/home/allowedRegions.test.ts
@@ -0,0 +1,82 @@
+import { isRegionAllowed } from './allowedRegions'
+import config from '../config'
+
+jest.mock('../config')
+
+describe('allowedRegions', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('isRegionAllowed', () => {
+    describe('when region restriction is enabled and ENVIRONMENT_NAME is not DEV', () => {
+      beforeEach(() => {
+        ;(config as jest.Mocked<typeof config>).enable_region_restriction = true
+        ;(config as jest.Mocked<typeof config>).environmentName = 'PREPROD'
+      })
+
+      it('should allow allowed region', () => {
+        expect(isRegionAllowed('N53')).toBe(true)
+      })
+
+      it('should not allow dev only region)', () => {
+        expect(isRegionAllowed('N01')).toBe(false)
+      })
+
+      it('should not allow unknown region', () => {
+        expect(isRegionAllowed('Y02')).toBe(false)
+      })
+
+      it('should not allow undefined region', () => {
+        expect(isRegionAllowed(undefined)).toBe(false)
+      })
+    })
+
+    describe('when region restriction is enabled and ENVIRONMENT_NAME is DEV', () => {
+      beforeEach(() => {
+        ;(config as jest.Mocked<typeof config>).enable_region_restriction = true
+        ;(config as jest.Mocked<typeof config>).environmentName = 'DEV'
+      })
+
+      it('should allow dev allowed region)', () => {
+        expect(isRegionAllowed('N01')).toBe(true)
+      })
+
+      it('should not allow unknown region', () => {
+        expect(isRegionAllowed('Y02')).toBe(false)
+      })
+
+      it('should not allow undefined region', () => {
+        expect(isRegionAllowed(undefined)).toBe(false)
+      })
+    })
+
+    describe('when region restriction is enabled and ENVIRONMENT_NAME is dev (lowercase)', () => {
+      beforeEach(() => {
+        ;(config as jest.Mocked<typeof config>).enable_region_restriction = true
+        ;(config as jest.Mocked<typeof config>).environmentName = 'dev'
+      })
+
+      it('should allow dev regions when environmentName is lowercase', () => {
+        expect(isRegionAllowed('N01')).toBe(true)
+      })
+    })
+
+    describe('when region restriction is disabled', () => {
+      beforeEach(() => {
+        ;(config as jest.Mocked<typeof config>).enable_region_restriction = false
+      })
+
+      it('should allow N53', () => {
+        expect(isRegionAllowed('N53')).toBe(true)
+      })
+      it('should allow unknown region ', () => {
+        expect(isRegionAllowed('Y02')).toBe(true)
+      })
+
+      it('should allow undefined region', () => {
+        expect(isRegionAllowed(undefined)).toBe(true)
+      })
+    })
+  })
+})
