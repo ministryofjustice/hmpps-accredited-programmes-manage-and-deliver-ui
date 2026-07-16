c059a97c96f366ef268fe37e106ebc673ba17657
Jack Dempster <5928690+Jack-Dempster@users.noreply.github.com>
2026-07-16 15:57:25 +0100

    Fix sentry implementation by ensuring its imported in app (#679)

--- server/sentry.ts
+++ server/sentry.ts
@@ -1,8 +1,31 @@
 import * as Sentry from '@sentry/node'
+import applicationInfo from './applicationInfo'
 import config from './config'
 
-Sentry.init({
-  dsn: config.sentry.dsn,
-  integrations: [Sentry.httpIntegration(), Sentry.expressIntegration],
-  environment: config.environmentName,
-})
+const { applicationName } = applicationInfo()
+
+if (config.sentry.dsn) {
+  Sentry.init({
+    dsn: config.sentry.dsn,
+    integrations: [Sentry.httpIntegration(), Sentry.expressIntegration()],
+    environment: config.sentry.environment,
+    release: `${applicationName}@${config.gitRef}`,
+    tracesSampler: samplingContext => {
+      if (
+        samplingContext.name.includes('ping') ||
+        samplingContext.name.includes('health') ||
+        samplingContext.name.includes('assets')
+      ) {
+        return 0
+      }
+      return config.sentry.tracesSampleRate
+    },
+    beforeSend(event) {
+      if (event.user?.username) {
+        // eslint-disable-next-line no-param-reassign
+        delete event.user.username
+      }
+      return event
+    },
+  })
+}
