bbdb78621c0b1fe9f1d107ac0f36996625ecd7f4
Alison-Weatherston <alison.weatherston@justice.gov.uk>
2026-07-21 09:16:38 +0100

    prettier complaint addressed

--- server/pni/pniController.ts
+++ server/pni/pniController.ts
@@ -6,7 +6,6 @@ import { PrimaryNavigationTab } from '../shared/routes/layoutPresenter'
 import BaseController from '../shared/baseController'
 import { setReferralOriginPage } from '../utils/referralOriginPage'
 
-
 export default class PniController extends BaseController {
   protected readonly primaryNavigationTab = PrimaryNavigationTab.Caselist
 
