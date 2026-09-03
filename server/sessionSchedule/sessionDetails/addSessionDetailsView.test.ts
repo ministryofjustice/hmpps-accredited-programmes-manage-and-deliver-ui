35091003751f74c7883f101bf5b5bb79fd54fe02
Alison-Weatherston <alison.weatherston@justice.gov.uk>
2026-09-03 08:22:52 +0100

    Time restricted to 2 digits per field (#754)
    * Time restricted to 2 digits per field
    
    * improved tests

---
+++ server/sessionSchedule/sessionDetails/addSessionDetailsView.test.ts
@@ -0,0 +1,42 @@
+import AddSessionDetailsView from './addSessionDetailsView'
+
+describe('time input fields', () => {
+  it('limits hour and minute values to two digits in session details inputs', () => {
+    const presenter = {
+      backLinkUri: '/back',
+      isGroupSession: false,
+      isCatchUpSession: false,
+      fields: {
+        sessionDate: { errorMessage: null, value: '01/01/2026' },
+        startTime: {
+          errorMessages: [],
+          hour: { hasError: false, value: '9' },
+          minute: { hasError: false, value: '30' },
+          partOfDay: { hasError: false, value: 'AM' },
+        },
+        endTime: {
+          errorMessages: [],
+          hour: { hasError: false, value: '10' },
+          minute: { hasError: false, value: '45' },
+          partOfDay: { hasError: false, value: 'PM' },
+        },
+      },
+    } as unknown as ConstructorParameters<typeof AddSessionDetailsView>[0]
+
+    const view = new AddSessionDetailsView(presenter)
+
+    expect(view.startTimeInputArgs.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ name: 'hour', attributes: { maxlength: '2' } }),
+        expect.objectContaining({ name: 'minute', attributes: { maxlength: '2' } }),
+      ]),
+    )
+
+    expect(view.endTimeInputArgs.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ name: 'hour', attributes: { maxlength: '2' } }),
+        expect.objectContaining({ name: 'minute', attributes: { maxlength: '2' } }),
+      ]),
+    )
+  })
+})
